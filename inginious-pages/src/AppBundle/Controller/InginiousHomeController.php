01025e1c0b28b1cb4c2d220cd5c4f4d40f040fbe
Ben Horowitz <ben@benhorowitz.com>
2015-12-04 18:19:00 -0800

    Refactor and integrate with album manager

--- inginious-pages/src/AppBundle/Controller/InginiousHomeController.php
+++ inginious-pages/src/AppBundle/Controller/InginiousHomeController.php
@@ -9,6 +9,8 @@
 // src/AppBundle/Controller/LuckyController.php
 namespace AppBundle\Controller;
 
+use AppBundle\Services\PhotoManager;
+use GuzzleHttp\Exception\RequestException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Config\Definition\Exception\Exception;
@@ -24,19 +26,25 @@ use Symfony\Component\Serializer\Encoder\JsonEncode;
 class InginiousHomeController extends Controller
 {
 
-    public function getPhotoManager()
-    {
-        $photoManagerURL = getenv("PHOTOMANAGER_ENDPOINT_URL");
-        $photoCatalogPath = getenv("PHOTOMANAGER_CATALOG_PATH");
-        $photoAlbumPath = getenv("PHOTOMANAGER_ALBUM_PATH");
-        //print $photoManagerURL . " | " . $photoCatalogPath . " | " . $photoAlbumPath;
-        return $photoManager = array(
-            //'url' => isset($_ENV["PHOTOMANAGER_ENDPOINT_URL"]) ? $_ENV["PHOTOMANAGER_ENDPOINT_URL"]: "metadogs.com";
-            //'catalog-path' => $_ENV["PHOTOMANAGER_CATALOG_PATH"],
-            //'album-path' => $_ENV["PHOTOMANAGER_ALBUM_PATH"]);
-            'url' => $photoManagerURL,
-            'catalog-path' => $photoCatalogPath,
-            'album-path' => $photoAlbumPath);
+    private $name;
+    private $firstName;
+    private $lastName;
+    private $photoManager = null;
+
+    public function isAuthenticated(Request $request) {
+        $authID = $request->headers->get('Auth-ID');
+
+        if ($authID != "") {
+            $this->name = $request->headers->get('Auth-Name');
+
+            $names = explode(' ', $this->name);
+            $this->firstName = $names[0];
+            $this->lastName = $names[1];
+
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -44,60 +52,22 @@ class InginiousHomeController extends Controller
      */
     public function indexAction(Request $request)
     {
-            return $this->render('/index.html.twig', array(
-                'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-            ));
-    }
+//        var_dump($this->getPhotoManager()->getCatalog(1)); exit;
 
-    /**
-     * @Route("/hello", name="hello")
-     */
-    public function homeAction(Request $request)
-    {
-        $cookies = $request->cookies;
-        $authID = $request->headers->get('Auth-ID');
-
-        if ($authID != "")
-        {
-            $Name = $request->headers->get('Auth-Name');
-            $Names = explode(" ",$Name);
-            $firstName = $Names[0];
-            $lastName = $Names[1];
-            $photoManager = $this->getPhotoManager();
-
-            try
-            {
-                $client = new Client(['base_uri' => 'http://' . $photoManager["url"]]);
-                $myJsonResponse = $client->request('GET', $photoManager["catalog-path"], [
-                    'query' => ['catalog' => 'orchids',
-                        'title' => 'Orchids']
-                ]);
-                $body = $myJsonResponse->getBody();
-                // Explicitly cast the body to a string
-                $stringBody = (string)$body;
-                $catalogDecoder = new JsonDecode();
-                $catalog = $catalogDecoder->decode($stringBody, 'json');
-            }
-            catch (RequestException $e)
-            {
-                echo $e->getRequest();
-                if ($e->hasResponse()) { echo $e->getResponse();}
-            }
+        if ($this->isAuthenticated($request)) {
             return $this->render(
                 '/home.html.twig',
-                array('firstName' => $firstName,
-                    'lastName' => $lastName,
+                [
+                    'firstName' => $this->firstName,
+                    'lastName' => $this->lastName,
                     'authenticated' => 'header',
                     'catalogID' => 'orchids',
-                    'catalog' => $catalog)
+                    'catalog' => $this->getPhotoManager()->getCatalog(1)
+                ]
             );
-            return new Response($html);
-        }
-        else
-        {
-            return $this->redirectToRoute('homepage');
         }
 
+        return $this->render('/index.html.twig');
     }
 
     /**
@@ -106,83 +76,34 @@ class InginiousHomeController extends Controller
 
     public function albumAction($catalog, $album, Request $request)
     {
-        $cookies = $request->cookies;
-        $authID = $request->headers->get('Auth-ID');
+        if ($this->isAuthenticated($request)) {
+            $album = $this->getPhotoManager()->getAlbum($album);
+            $images = $album->images;
 
-        if ($authID != "")
-        {
-            $Name = $request->headers->get('Auth-Name');
-            $Names = explode(" ",$Name);
-            $firstName = $Names[0];
-            $lastName = $Names[1];
-            $photoManager = $this->getPhotoManager();
-
-            try
-            {
-                $client = new Client(['base_uri' => 'http://' . $photoManager["url"]]);
-                $myJsonResponse = $client->request('GET', $photoManager["album-path"], [
-                    'query' => ['folder' => $catalog,
-                                'title' => $album,
-                                'photoSet' => $album]
-                ]);
-                $body = $myJsonResponse->getBody();
-                // Explicitly cast the body to a string
-                $stringBody = (string)$body;
-                $albumDecoder = new JsonDecode();
-                $albumMap = $albumDecoder->decode($stringBody, 'json');
-                $images = $albumMap[0]->images;
-            }
-            catch (RequestException $e)
-            {
-                echo $e->getRequest();
-                if ($e->hasResponse()) { echo $e->getResponse();}
-            }
             return $this->render(
                 '/album.html.twig',
-                array('firstName' => $firstName,
-                    'lastName' => $lastName,
+                [
+                    'firstName' => $this->firstName,
+                    'lastName' => $this->lastName,
                     'authenticated' => 'header',
                     'catalogID' => $catalog,
-                    'album' => $albumMap[0],
-                    'images' => $images)
+                    'album' => $album,
+                    'images' => $images
+                ]
             );
-            return new Response($html);
-        }
-        else
-        {
+        } else {
             return $this->redirectToRoute('homepage');
         }
     }
 
     /**
-     * @Route("/lucky/number/{count}")
+     * @return PhotoManager
      */
-    public function numberAction($count)
-    {
-        $numbers = array();
-        for ($i = 0; $i < $count; $i++) {
-            $numbers[] = rand(0, 100);
+    private function getPhotoManager() {
+        if ($this->photoManager == null) {
+            $this->photoManager = new PhotoManager();
         }
-        $numbersList = implode(', ', $numbers);
 
-        return $this->render(
-            '/home.html.twig',
-            array('luckyNumberList' => $numbersList)
-        );
-
-        return new Response($html);
-    }
-
-    /**
-     * @Route("/api/lucky/number")
-     */
-    public function apiNumberAction()
-    {
-        $data = array(
-            'lucky_number' => rand(0, 100),
-        );
-
-        // calls json_encode and sets the Content-Type header
-        return new JsonResponse($data);
+        return $this->photoManager;
     }
 }
---
+++ inginious-pages/src/AppBundle/Services/PhotoManager.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benhorowitz
+ * Date: 12/4/15
+ * Time: 3:04 PM
+ */
+
+namespace AppBundle\Services;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Symfony\Component\Serializer\Encoder\JsonDecode;
+
+/**
+ * Class PhotoManager
+ * @package AppBundle\Services
+ */
+class PhotoManager
+{
+    /**
+     * @var string
+     */
+    private $url;
+    /**
+     * @var string
+     */
+    private $catalogPath;
+    /**
+     * @var string
+     */
+    private $albumPath;
+    /**
+     * @var Client
+     */
+    private $client = null;
+
+    /**
+     * PhotoManager constructor.
+     */
+    public function __construct() {
+        $this->url = getenv("PHOTOMANAGER_ENDPOINT_URL");
+        $this->catalogPath = getenv("PHOTOMANAGER_CATALOG_PATH");
+        $this->albumPath = getenv("PHOTOMANAGER_ALBUM_PATH");
+    }
+
+    /**
+     * @param $userId
+     * @return string
+     */
+    public function getCatalog($userId) {
+        $params = [
+            'query' => [
+                'user_id' => $userId
+            ]
+        ];
+
+        return $this->getRequest($this->catalogPath, $params);
+    }
+
+    /**
+     * @param $albumId
+     * @return string
+     */
+    public function getAlbum($albumId) {
+        $path = $this->albumPath . '/' . $albumId;
+
+        return $this->getRequest($path);
+    }
+
+    /**
+     * @return Client
+     */
+    private function getClient() {
+        if ($this->client == null) {
+            $this->client = new Client(['base_uri' => 'http://' . $this->url]);
+        }
+
+        return $this->client;
+    }
+
+    /**
+     * @param $path
+     * @param $params
+     * @return string
+     */
+    private function getRequest($path, $params = []) {
+        try
+        {
+            $client = $this->getClient();
+            $response = $client->request('GET', $path, $params);
+
+            $body = $response->getBody()->__toString();
+
+            $decoder = new JsonDecode();
+            return $decoder->decode($body, 'json');
+        }
+        catch (RequestException $e)
+        {
+            echo $e;
+        }
+    }
+}

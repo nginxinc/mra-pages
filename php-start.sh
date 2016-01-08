d20ef624adb0b3466b1998363470fe7bbd9b9859
Chris Stetson <chris.stetson@nginx.com>
2016-01-07 19:33:52 -0800

    adding status.html

--- php-start.sh
+++ php-start.sh
@@ -2,7 +2,9 @@
 pid="/var/run/nginx.pid"    # /   (root directory)
 
 nginx -c /etc/nginx/nginx-php.conf -g "pid $pid;" &
+
 service amplify-agent start
+
 php-fpm -y /etc/php5/fpm/php-fpm.conf -R
 
 sleep 500

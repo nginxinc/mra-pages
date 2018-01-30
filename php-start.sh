6125a7f9d5e5a74d8285beb95fe36ae4a22b23ba
Aidan Carson <aidan.carson@nginx.com>
2018-01-29 18:27:06 -0800

    MRADEV-696 Add Pages
    Added support for pages, as well as added environment variables in ingenious.js so that the app can correctly route requests to the different services.

--- php-start.sh
+++ php-start.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-pid="/var/run/nginx.pid";    # /   (root directory)
 fpm_pid="/var/run/php-fpm.pid";
-nginx_conf="/etc/nginx/nginx.conf";
-
-echo "Starting ${nginx_conf} with pid ${pid}"
-
-nginx -c "$nginx_conf" -g "pid $pid;" &
 
 echo "FPM PID ${fpm_pid}"
 
@@ -20,7 +14,7 @@ php-fpm -y /etc/php5/fpm/php-fpm.conf -R &
 echo launched processes;
 sleep 10;
 
-while [ -f "$pid" ] &&  [ -f "$fpm_pid" ];
+while [ -f "$fpm_pid" ];
 do
 	sleep 5;
     #echo in the while loop;

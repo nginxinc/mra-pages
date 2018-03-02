05131530b85c77a8be6018e336f07577033fbac6
Aidan Carson <aidan.carson@nginx.com>
2018-03-02 10:16:26 -0800

    MRADEV-820 Adjust Article Insert
    Updated article insert script to prevent unnecessary messages from getting pushed into the log. Also added a timeout.

--- ingenious-pages/Insert.php
+++ ingenious-pages/Insert.php
@@ -13,9 +13,15 @@ $uploader_path = getenv("PHOTOUPLOADER_IMAGE_PATH");
 
 // Wait for response from uploader
 $responseCode = 0;
+$startTime = time();
 while ($responseCode == "0" || $responseCode == "502"){
     $responseCode = connect("https:/" . $uploader_path);
-    sleep(5);
+    sleep(10);
+
+    if (time() - $startTime > 120){
+        trigger_error("Unable to insert posts", E_USER_ERROR);
+        return false;
+    }
 }
 
 // GET articles to verify articles in DB
@@ -163,6 +169,7 @@ function connect($url){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_exec($ch);
     return curl_getinfo($ch, CURLINFO_HTTP_CODE);
